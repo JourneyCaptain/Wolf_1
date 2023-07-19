129599e253ca631390d3b9fd35d6149e42a70126
nasta <nastas799@gmail.com>
2023-07-19 13:57:32 +0200

    Test

--- ClassWork/Class_Work_Lektion/Lektion_21/practice/finde_Book/library/LibraryAppl.java
+++ ClassWork/Class_Work_Lektion/Lektion_21/practice/finde_Book/library/LibraryAppl.java
@@ -3,7 +3,7 @@ import Lektion_21.practice.finde_Book.models.Library;
 public class LibraryAppl {
     public static void main(String[] args) {
 
-          Library library = new Library(1000);
+        Library library = new Library(1000);
         library.addBook(new Book("War and Peace", "Lev Tolstoi", 1990,2000000000000L));
         library.addBook(new Book("Anna Karenina", "Lev Tolstoi", 1995,2000000000001L));
         library.addBook(new Book("1984", "Oruell", 1985,2000000000002L));
