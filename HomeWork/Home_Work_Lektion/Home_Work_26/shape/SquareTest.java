869a05311ff6c17187b910c26ef0ec9b14270dfe
nasta <nastas799@gmail.com>
2023-07-25 21:03:02 +0200

    test

---
+++ HomeWork/Home_Work_Lektion/Home_Work_26/shape/SquareTest.java
@@ -0,0 +1,21 @@
+package Home_Work_26.shape;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquareTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void calcArea() {
+    }
+
+    @Test
+    void calcPerimeter() {
+    }
+}
